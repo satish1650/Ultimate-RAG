78b1724725e4c6a0e7c58d3bde30b52c0a2cd301
Satish Tiwari <satishsjce86@gmail.com>
2025-11-16 12:40:59 +0530

    Complete Class Notes

--- Material/UltimateRAG/Complete Class Notes.lua
+++ Material/UltimateRAG/Complete Class Notes.lua
@@ -9,8 +9,9 @@ https://learn.krishnaikacademy.com/web/checkout/68bbbf371263fba553cd0492
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 08 Nov Day - 2 Tools And Installation
 *************************************
-Ultimate-RAG-Course-Checklist:
-https://krishnaikacademy.notion.site/Ultimate-RAG-Course-2a5eba9593d08085ade8ceb2a6c6c8de
+Class Notes :- https://krishnaikacademy.notion.site/Software-Checklist-2a5eba9593d08048927ed6fbd00f502d?pvs=143
+
+Ultimate-RAG-Course-Checklist :- https://krishnaikacademy.notion.site/Ultimate-RAG-Course-2a5eba9593d08085ade8ceb2a6c6c8de
 
 Anaconda Installation Guide:
 https://www.anaconda.com/download/success
@@ -31,26 +32,24 @@ Creating the Virtual Env using conda
 
 	conda deactivate
 
-uv Installation Guide:
-https://docs.astral.sh/uv/#installation
+uv Installation Guide :- https://docs.astral.sh/uv/#installation
 
-warp Installation Guide:
-A new way to code 👉 The Agentic Development Environment
+warp Installation Guide :- A new way to code 👉 The Agentic Development Environment
 https://www.warp.dev/
 
 Set-ExecutionPolicy  -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
-Git Installation Guide:
-https://git-scm.com/install/windows
+Git Installation Guide :- https://git-scm.com/install/windows
 
-Install Docker Desktop on Windows:-
-https://docs.docker.com/desktop/setup/install/windows-install/
+Install Docker Desktop on Windows :- https://docs.docker.com/desktop/setup/install/windows-install/
 
 app@krishnaik.in
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 09 Nov Day - 3 Introduction To RAG
 **********************************
+Class Notes :- https://krishnaikacademy.notion.site/Introduction-to-RAG-2a6eba9593d080e88876ded8c13e0627?pvs=143
+
 Paper Reserach
 https://krishnaikacademy.notion.site/Ultimate-RAG-Course-2a5eba9593d08085ade8ceb2a6c6c8de?pvs=74
 
@@ -66,17 +65,25 @@ Speech and Natural Language Processing (NLP Book)
 https://web.stanford.edu/~jurafsky/slp3/
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
 15 Nov Day - 4 RAG Architectures & Survey
 *****************************************
-Class Notes :- https://drive.google.com/file/d/1_uRq1ZjtjNH7abY_bsHcS2JcJm83xkpn/view?usp=sharing
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+Class Notes :- https://krishnaikacademy.notion.site/RAG-Detailed-Survey-2aceba9593d080d18acdf9a97ad9cfdd?pvs=143
+
+Class Notes :- https://drive.google.com/file/d/1_uRq1ZjtjNH7abY_bsHcS2JcJm83xkpn/view?usp=sharing
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 16 Nov Day - 5 Exploring Langchain For RAG
 ******************************************
+Class Notes :- https://krishnaikacademy.notion.site/Exploring-Langchain-2adeba9593d080a9ba65f36aba9e283e?pvs=143
+
 Promptly AI -> Prompt Library
 https://www.promptly.fyi/library
 
 simple-rag-langchain.zip
 Code Download Link :- https://drive.google.com/file/d/1gH9PaMtZUKb5-5nJ98s23ZkRzkJF6Zda/view?usp=sharing
+
+ChunkViz v0.1
+https://chunkviz.up.railway.app/
+
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
