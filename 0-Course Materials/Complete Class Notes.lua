fab3eb457db7bd20f81ce26caf18dfc12f18735b
Satish Tiwari <satishsjce86@gmail.com>
2026-05-03 10:41:09 +0530

    Complete Class Notes - 03-May-2026

--- 0-Course Materials/Complete Class Notes.lua
+++ 0-Course Materials/Complete Class Notes.lua
@@ -1732,7 +1732,7 @@ client = OpenAI(
 response = client.chat.completions.create(
     model="ai21/jamba-1-5-large-v1",
     messages=[
-        {"role": "user", "content": "Who won IPL match on 02 May 2026? and who was the highest run scorer in the same match?"}
+        {"role": "user", "content": "Hello"}
     ]
 )
 
